93356bf5a089e4aaf3cf6d8e647c1975082d1879
elbse <changpriego@gmail.com>
2024-12-07 21:09:36 +0800

    Backticks

--- main.js
+++ main.js
@@ -2,6 +2,20 @@ let age = 20;
 
 console.log("Im currently " + age + " years old.");
 
-const name = "Judiann";
 
-console.log(name + age);
+//Creating and object 
+
+let person = {
+    name: "Bernie",
+    age: 19,
+    city: "Davao City",
+    greet: function(){
+        return `Hello, my name is ${this.name} and I live ${this.city}.`;
+    }
+};
+
+console.log(person.name);
+console.log(person["age"]);
+
+console.log(person.greet());
+
