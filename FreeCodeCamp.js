8918ea35aa794bc2dfc75a110e292764b556d05d
elbse <changpriego@gmail.com>
2024-12-08 22:55:26 +0800

    k

---
+++ FreeCodeCamp.js
@@ -0,0 +1,23 @@
+var number = 5; // in-line comment 
+
+/* long comment 
+Data Types : undefined, null, boolean, string, symbol, number, and object - key value pairs
+Variable - a label to point a data 
+*/ 
+
+//Declaring variables  
+var myName = "Charisse"; // Used in the whole program 
+
+let ourName = "freeCodeCamp" // You can only use this where you declare this 
+
+const pi = 3.14 // Variable that cannot be change
+
+var a; //Declaring Var
+var b = 2; //Declaring and assigning variable
+
+a = 7;
+
+b = a;
+
+console.log(a);
+
---
+++ MakingDecisions.js
@@ -0,0 +1,9 @@
+// If condition 
+
+let currentMoney = 2300;
+let laptopPrice = 1500;
+
+if (currentMoney >= laptopPrice){
+    console.log("Getting a new Laptop.")
+}
+
